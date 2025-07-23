25d92b338e3eca2be64a643e5085bde005b79270
Jasmine Tu <jasminetntu@gmail.com>
2025-07-23 17:20:28 +0000

    linked cloudsploit as a tool for vulnerability agent
    currently works only if the cloudsploit repository is inside the 'with_subagents' folder. currently trying to implement cloud run to separate the repos.
    
    updated readme to reflect status

--- with_subagents/main_agent/agent.py
+++ with_subagents/main_agent/agent.py
@@ -19,7 +19,7 @@ from .subagents.sqlAgent import sql_agent
 from .subagents.bqAgent import bq_agent
 from .subagents.gceAgent import gce_agent
 from .subagents.gcsAgent import gcs_agent
-#from .subagents.searchAgent import search_agent
+from .subagents.vulnerabilityAgent import vulnerability_agent
 
 root_agent = LlmAgent(
     name="gcp_coordinator",
@@ -28,16 +28,18 @@ root_agent = LlmAgent(
         "Guide user query to the relevant agent based on the quesion about a GCP product. "
     ),
     instruction=
-    """    
-    - Let the user know you will help them with a question about a Google Cloud Platform product.
-    - When they respond, understand their query and transfer to one of 
-     gce_agent for Google Compute Engine or compute-related question,
-     gcs_agent for Google Cloud Storage related questiosn,
-     bq_agent for BigQuery related questions,
-     and sql_agent for Cloud SQL related questions.
-     For all else, say "Currently not supported."
-     """,
-    sub_agents=[gce_agent, gcs_agent, bq_agent, sql_agent],
+        """    
+        - Let the user know you will help them with a question about a Google Cloud Platform product 
+        OR scanning vulnerabilities in the user's Google Cloud project.
+        - When they respond, understand their query and transfer to one of these agents:
+        gce_agent for Google Compute Engine or compute-related question,
+        gcs_agent for Google Cloud Storage related questiosn,
+        bq_agent for BigQuery related questions,
+        sql_agent for Cloud SQL related questions,
+        and vulnerability_agent for scanning vulnerabilities.
+        For all else, say "Currently not supported."
+        """,
+    sub_agents=[gce_agent, gcs_agent, bq_agent, sql_agent, vulnerability_agent],
 )
 
 
