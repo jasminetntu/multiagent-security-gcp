10b11be4e900315210611f0dab9d8c10cb36c325
enamacahiya <enacjmac@gmail.com>
2025-07-28 21:33:29 +0000

    created prompt.py + updated agents.py for answer/search subagents

--- with_subagents/main_agent/subagents/answerAgent/agent.py
+++ with_subagents/main_agent/subagents/answerAgent/agent.py
@@ -1,13 +1,30 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""answer_agent for providing answers to user queries."""
+
 from google.adk import Agent
 from google.adk.tools.agent_tool import AgentTool
 from google.genai import types
+from . import prompt
 
 from ..searchAgent import search_agent
 
 MODEL = "gemini-2.5-flash"
 
 def answer_request(request: str) -> dict:
-    """ 
+    """
     Answers user's request to best of ability.
 
     Args: request
@@ -23,12 +40,11 @@ def answer_request(request: str) -> dict:
 answer_agent = Agent(
     name="answer_agent",
     model=MODEL,
-    description="Helpful agent that answers Google Cloud related questions.",
-    instruction="""
-                You are a helpful agent that specializes in answering questions and providing detailed information related to Google Cloud Platform.
-                If the question is within your scope, call answer_request.
-                If you are unsure of the answer, call the search_agent to perform a google search.
-                """,
-    tools=[answer_request,
-            AgentTool(search_agent)],
+    description=prompt.ANSWER_AGENT_DESCRIPTION,  # Using description from prompt.py
+    instruction=prompt.ANSWER_AGENT_INSTRUCTION,  # Using instruction from prompt.py
+    output_key="final_answer_output",             # Added output_key for session state
+    tools=[
+        answer_request,
+        AgentTool(search_agent) # This allows answer_agent to call search_agent
+    ],
 )
--- with_subagents/main_agent/subagents/searchAgent/agent.py
+++ with_subagents/main_agent/subagents/searchAgent/agent.py
@@ -1,13 +1,30 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""search_agent for finding information using google search"""
+
 from google.adk import Agent
-from google.genai import types
 from google.adk.tools import google_search
+from . import prompt
 
 MODEL = "gemini-2.5-flash"
 
 search_agent = Agent(
-    name="search_agent",
     model=MODEL,
-    description="Professional agent that performs a google search given a request.",
-    instruction="You are an agent that performs a Google Search given the user's request. Always cite sources.",
+    name="search_agent",
+    description=prompt.SEARCH_AGENT_DESCRIPTION,  # Using description from prompt.py
+    instruction=prompt.SEARCH_AGENT_INSTRUCTION,  # Using instruction from prompt.py
+    output_key="search_results_output",          # Consistent output_key
     tools=[google_search],
 )
